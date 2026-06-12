5f2005128a29bf02e2e2746c4054fd34969292cb
Bakamashine <ivan.fa.002@gmail.com>
2026-06-12 20:22:47 +0300

    add a message with not found

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -17,43 +17,45 @@ export default function Home() {
           <p className="lead">Кроссплатформенность - сила</p>
         </Col>
       </Row>
-      <Row xs={1} md={2} lg={3} xl={4} className="g-4">
-        {recordStore.records.map((item) => (
-          <Col key={item.id}>
-            <Card className="h-100">
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                  {item.markdown.slice(0, 10).concat("...")}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <Link
-                  className="btn btn-info m-2"
-                  to={`/record/${item.id}/edit`}
-                >
-                  Редактировать
-                </Link>
-                <Link className="btn btn-info m-2" to={`/record/${item.id}`}>
-                  Подробнее
-                </Link>
-                <Button
-                  variant="danger"
-                  className="m-2"
-                  onClick={async () => {
-
-                    await recordStore.deleteRecord(item.id)
-                    await recordStore.fetchRecords()
-                  }
-                  }
-                >
-                  Удалить
-                </Button>
-              </Card.Footer>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      {recordStore.records && recordStore.records.length > 0 ? (
+        <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+          {recordStore.records.map((item) => (
+            <Col key={item.id}>
+              <Card className="h-100">
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>
+                    {item.markdown.slice(0, 10).concat("...")}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link
+                    className="btn btn-info m-2"
+                    to={`/record/${item.id}/edit`}
+                  >
+                    Редактировать
+                  </Link>
+                  <Link className="btn btn-info m-2" to={`/record/${item.id}`}>
+                    Подробнее
+                  </Link>
+                  <Button
+                    variant="danger"
+                    className="m-2"
+                    onClick={async () => {
+                      await recordStore.deleteRecord(item.id);
+                      await recordStore.fetchRecords();
+                    }}
+                  >
+                    Удалить
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <p className="text-center">Ничего не найдено</p>
+      )}
     </Container>
   );
 }
